9505b028379e194ff292d2bf59984b5c0fef6af7
Vinay_Sharma <vinay@elred.io>
2024-02-27 11:22:58 +0530

    "compute func"

--- lib/home/web_service/home_screen_web_service.dart
+++ lib/home/web_service/home_screen_web_service.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:dio_training/home/model/user_model.dart';
+import 'package:flutter/foundation.dart';
 
 class HomeWebService {
   final Dio _dio = Dio(
@@ -12,14 +13,19 @@ class HomeWebService {
 
   Future<List<UserModel>?> fetchUsers() async {
     try {
-      final Response response = await _dio.get("users");
+      final Response response = await _dio.get("allUsers");
       if (response.statusCode != 200) throw response;
       if (response.data == null) throw response;
       final List data = (response.data);
-      return data.map((e) => UserModel.fromMap(e)).toList();
+      return await compute(_parseData, data);
     } catch (e) {
       log("Error $e");
       return null;
     }
   }
+}
+
+// Top level func
+List<UserModel> _parseData(List data) {
+  return data.map((e) => UserModel.fromMap(e)).toList();
 }
